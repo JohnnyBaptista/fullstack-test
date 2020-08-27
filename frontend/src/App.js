2c5ed9b6199c822525c0954056c08cc933a98680
JP Baptista <joaosbaptista@hotmail.com>
2020-08-27 20:18:09 -0300

    build: criando a base do projeto

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AppRoutes from "./app.routes";
+import { AppContainer } from "./styles/styles";
+
+function App() {
+  return (
+    <AppContainer>
+      <AppRoutes />
+    </AppContainer>
+  );
+}
+
+export default App;
